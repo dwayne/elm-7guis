01f11775dd62f56cccbd74352df363e07b22d0d6
Dwayne Crooks <me@dwaynecrooks.com>
2023-06-10 06:09:35 -0400

    Cells: Evaluate an application

--- src/Cells/Data/Cell.elm
+++ src/Cells/Data/Cell.elm
@@ -8,9 +8,10 @@ module Cells.Data.Cell exposing
 
 import Cells.AST as AST
 import Cells.Data.Coord exposing (Coord)
-import Cells.Data.Range exposing (Range)
+import Cells.Data.Range as Range exposing (Range)
 import Cells.Data.SCells as SCells exposing (SCells)
 import Cells.Parser as P
+import Dict exposing (Dict)
 import Parser exposing (DeadEnd)
 
 
@@ -38,6 +39,12 @@ type Value
 type RuntimeError
     = TopLevelRange Range
     | IdentifierNotFound String
+    | WrongNumberOfArguments
+        String
+        { expected : Int
+        , given : Int
+        }
+    | DivisionByZero
 
 
 empty : Coord -> Cell
@@ -82,14 +89,135 @@ evaluate scells expr =
             Ok x
 
         AST.Cell coord ->
-            Ok <| toFloat <| SCells.get empty coord scells
+            Ok <| evaluateCoord scells coord
 
         AST.Range range ->
             Err <| TopLevelRange range
 
-        AST.Application identifier _ ->
-            -- TODO: Evaluate application.
-            Err <| IdentifierNotFound identifier
+        AST.Application identifier exprs ->
+            case Dict.get identifier builtinFunctions of
+                Just f ->
+                    evaluateArgs scells exprs
+                        |> Result.andThen f
+
+                Nothing ->
+                    Err <| IdentifierNotFound identifier
+
+
+evaluateCoord : SCells Cell -> Coord -> Float
+evaluateCoord scells coord =
+    toFloat <| SCells.get empty coord scells
+
+
+evaluateArgs : SCells Cell -> List AST.Expr -> Result RuntimeError (List Float)
+evaluateArgs scells =
+    concatMapSequence (evaluateArg scells)
+
+
+concatMapSequence : (a -> Result e (List b)) -> List a -> Result e (List b)
+concatMapSequence =
+    concatMapSequenceHelper []
+
+
+concatMapSequenceHelper : List b -> (a -> Result e (List b)) -> List a -> Result e (List b)
+concatMapSequenceHelper acc f list =
+    case list of
+        [] ->
+            Ok <| List.reverse acc
+
+        a :: restList ->
+            case f a of
+                Ok bs ->
+                    concatMapSequenceHelper (bs ++ acc) f restList
+
+                err ->
+                    err
+
+
+evaluateArg : SCells Cell -> AST.Expr -> Result RuntimeError (List Float)
+evaluateArg scells expr =
+    case expr of
+        AST.Range range ->
+            range
+                |> Range.expand
+                |> List.map (evaluateCoord scells)
+                |> Ok
+
+        _ ->
+            evaluate scells expr
+                |> Result.map List.singleton
+
+
+builtinFunctions : Dict String (List Float -> Result RuntimeError Float)
+builtinFunctions =
+    Dict.fromList
+        [ ( "add"
+          , \args ->
+                case args of
+                    x :: y :: [] ->
+                        Ok <| x + y
+
+                    _ ->
+                        Err <| WrongNumberOfArguments "add" { expected = 2, given = List.length args }
+          )
+        , ( "sub"
+          , \args ->
+                case args of
+                    x :: y :: [] ->
+                        Ok <| x - y
+
+                    _ ->
+                        Err <| WrongNumberOfArguments "sub" { expected = 2, given = List.length args }
+          )
+        , ( "mul"
+          , \args ->
+                case args of
+                    x :: y :: [] ->
+                        Ok <| x * y
+
+                    _ ->
+                        Err <| WrongNumberOfArguments "mul" { expected = 2, given = List.length args }
+          )
+        , ( "div"
+          , \args ->
+                case args of
+                    x :: y :: [] ->
+                        if y /= 0 then
+                            Ok <| x / y
+
+                        else
+                            Err DivisionByZero
+
+                    _ ->
+                        Err <| WrongNumberOfArguments "div" { expected = 2, given = List.length args }
+          )
+        , ( "mod"
+          , \args ->
+                case args of
+                    x :: y :: [] ->
+                        let
+                            m =
+                                truncate x
+
+                            n =
+                                truncate y
+                        in
+                        if n /= 0 then
+                            Ok <| Basics.toFloat (m |> modBy n)
+
+                        else
+                            Err DivisionByZero
+
+                    _ ->
+                        Err <| WrongNumberOfArguments "mod" { expected = 2, given = List.length args }
+          )
+        , ( "sum"
+          , Ok << List.sum
+          )
+        , ( "prod"
+          , Ok << List.product
+          )
+        ]
 
 
 toFloat : Cell -> Float
--- src/Cells/Data/Range.elm
+++ src/Cells/Data/Range.elm
@@ -1,9 +1,40 @@
-module Cells.Data.Range exposing (Range)
+module Cells.Data.Range exposing (Range, expand)
 
+import Cells.Data.Column as Column
 import Cells.Data.Coord exposing (Coord)
+import Cells.Data.Row as Row
 
 
 type alias Range =
     { start : Coord
     , end : Coord
     }
+
+
+expand : Range -> List Coord
+expand { start, end } =
+    let
+        startColumn =
+            Column.toInt start.column
+
+        startRow =
+            Row.toInt start.row
+
+        endColumn =
+            Column.toInt end.column
+
+        endRow =
+            Row.toInt end.row
+
+        columns =
+            List.range startColumn endColumn
+                |> List.map Column.fromSafeInt
+
+        rows =
+            List.range startRow endRow
+                |> List.map Row.fromSafeInt
+
+        coordsForColumn c =
+            List.map (Coord c) rows
+    in
+    List.concatMap coordsForColumn columns
